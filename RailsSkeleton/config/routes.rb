403b471314a1c65cec62eb54f1d8c488262a5c15
Ryan Briones <ryan.briones@brionesandco.com>
2014-05-16 10:02:57 -0500

    CODE REVIEW, Ryan

--- RailsSkeleton/config/routes.rb
+++ RailsSkeleton/config/routes.rb
@@ -1,11 +1,12 @@
 RailsSkeleton::Application.routes.draw do
 
-
   get 'game/game'
 
   root to: 'sessions#index'
 
   match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
+  # CODE REVIEW: Do I get an information telling me the auth has failed
+  # when I'm redirected to auth/failure?
   match 'auth/failure', to: redirect('/'), via: [:get, :post]
   match 'signout', to: 'sessions#destroy', as: 'signout', via: [:get, :post]
 end
